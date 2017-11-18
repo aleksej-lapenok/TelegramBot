19e3cc04603f2617539248ca37a1cde9ed71638d
aleksej <lapenok.aleksej@gmail.com>
2017-11-18 15:34:23 +0300

    update interfaces

--- src/main/kotlin/ru/ifmo/telegram/bot/services/game/GameFactory.kt
+++ src/main/kotlin/ru/ifmo/telegram/bot/services/game/GameFactory.kt
@@ -3,8 +3,8 @@ package ru.ifmo.telegram.bot.services.game
 import ru.ifmo.telegram.bot.entity.Player
 
 interface GameFactory<in T : Step, out E : Game<T>> {
-    fun maxCountPlayers(): Int
-    fun minCountPlayers(): Int
+    fun maxNumberPlayers(): Int
+    fun minNumberPlayers(): Int
     fun getGame(vararg player: Player): E
     fun getInfo(): String
     fun fromJson(json: String): E
