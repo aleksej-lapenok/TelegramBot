5f8023ef27ad4082edcdadf840b2f2b95965afeb
Aleksej Lapenok <lapenok.aleksej@gmail.com>
2017-12-14 16:53:17 +0300

    make more beautiful code

--- src/main/kotlin/ru/ifmo/telegram/bot/services/main/UpdateRequest.kt
+++ src/main/kotlin/ru/ifmo/telegram/bot/services/main/UpdateRequest.kt
@@ -8,6 +8,7 @@ import ru.ifmo.telegram.bot.repository.PlayerRepository
 import ru.ifmo.telegram.bot.services.game.Game
 import ru.ifmo.telegram.bot.services.game.Step
 import ru.ifmo.telegram.bot.services.telegramApi.TelegramSender
+import ru.ifmo.telegram.bot.services.telegramApi.Update
 import ru.ifmo.telegram.bot.services.telegramApi.UpdatesCollector
 
 @Service
@@ -20,7 +21,7 @@ class UpdateRequest(
     private val logger = LoggerFactory.getLogger(this.javaClass)
     private var lastUpdate = 0L
     private val games = mutableMapOf<Long, Game<*>>()
-    private val query = Games.values().toMutableList().map { it.name to mutableSetOf<Long>() }.toMap()
+    private val query = Games.values().toMutableList().map { it.name to mutableSetOf<Player>() }.toMap()
 
     @Scheduled(fixedDelay = 1000)
     fun getUpdates() {
@@ -28,19 +29,18 @@ class UpdateRequest(
         val result = updatesCollector.getUpdates(response)
         lastUpdate = result.maxBy { it.update_id }?.update_id ?: lastUpdate
         for (update in result) {
+            val player = getOrCreatePlayer(update)
             logger.info(update.data)
             if (update.data.startsWith("/start")) {
-                if (playerRepository.findByChatId(update.chatId) == null) {
-                    playerRepository.save(Player(name = update.name!!, chatId = update.chatId))
-                }
-                val player = playerRepository.findByChatId(update.chatId)
-                val text = player!!.name + " registered"
-                telegramSender.sendMessage(player.chatId, text)
+                val text = player.name + " registered"
+                sendToPlayer(player, text)
                 continue
             }
             if (update.data.startsWith("/game")) {
-                if (games[update.chatId] != null) {
-                    telegramSender.sendMessage(update.chatId, "You should finish previouse game, before start new")
+
+                var game = getGameByPlayer(player)
+                if (game != null) {
+                    sendToPlayer(player, "You should finish previouse game, before start new")
                     continue
                 }
                 val name = update.data.split(" ")[1]
@@ -48,34 +48,29 @@ class UpdateRequest(
                     logger.info("UnKnown game")
                     continue
                 }
-                val factory = mainGameFactory.getGameFactory(name)
-                query[name]!!.add(update.chatId)
-                if (query[name]?.size!! >= factory!!.minNumberPlayers()) {
-                    val playes = query[name]!!.toMutableList()
-                    query[name]!!.clear()
-                    val game = factory.getGame(*playes.map { playerRepository.findByChatId(it)!! }.toTypedArray())
-                    playes.forEach {
-                        games.put(it, game)
-                        query.values.forEach { it2 -> it2.remove(it) }
-                        telegramSender.sendMessage(it, game.getMessage(playerRepository.findByChatId(it)!!))
-                    }
+
+                addPlayerInQuery(player, name)
+                game = tryToGetNewGame(name)
+                if (game == null) {
+                    sendToPlayer(player, "waiting other playes")
                 } else {
-                    telegramSender.sendMessage(update.chatId, "waiting other playes")
+                    game.getPlayes().forEach {
+                        sendToPlayer(it, game.getMessage(it))
+                    }
                 }
+
                 continue
             }
             if (update.data.startsWith("/surrender")) {
-                if (games[update.chatId] == null) {
-                    logger.info("You should start game")
-                    telegramSender.sendMessage(update.chatId, "You should start game before you surrender")
+                val game = getGameByPlayer(player)
+                if (game == null) {
+                    sendToPlayer(player, "You should start game before you surrender")
                     continue
                 }
-                val game = games[update.chatId]!!
-                val player = playerRepository.findByChatId(chatId = update.chatId)!!
                 game.surrender(player)
-                telegramSender.sendMessage(update.chatId, "You left this game")
-                for (p in game.getPlayes()) {
-                    telegramSender.sendMessage(p.chatId, game.getMessage(p))
+                sendToPlayer(player, "You left this game")
+                game.getPlayes().forEach {
+                    sendToPlayer(it, game.getMessage(it))
                 }
                 games.remove(update.chatId)
                 continue
@@ -97,9 +92,44 @@ class UpdateRequest(
             if (update.data.startsWith("/help")) {
                 telegramSender.sendMessage(update.chatId, "Use commands: /game <nameGame> to start game\n /turn <arguments of turn> to make turn \n /surrender to exit from game")
                 telegramSender.sendMessage(update.chatId, "Game names: ${Games.values().map { it.name }}")
+                continue
             }
             telegramSender.sendMessage(update.chatId, "Unknown command: ${update.data}")
         }
     }
+
+    fun sendToPlayer(player: Player, message: String) = telegramSender.sendMessage(player.chatId, message)!!
+
+    fun addPlayerInGame(player: Player, game: Game<*>) {
+        games[player.chatId!!] = game
+    }
+
+    fun tryToGetNewGame(name: String): Game<*>? {
+        val factory = mainGameFactory.getGameFactory(name)
+        return if (query[name]?.size!! >= factory!!.minNumberPlayers()) {
+            val playes = query[name]!!.toMutableList()
+            query[name]!!.clear()
+            val game = factory.getGame(*playes.toTypedArray())
+            playes.forEach {
+                addPlayerInGame(it, game)
+            }
+            game
+        } else {
+            null
+        }
+    }
+
+    fun getOrCreatePlayer(update: Update): Player {
+        var player = playerRepository.findByChatId(update.chatId)
+        if (player == null) {
+            player = Player(name = update.name, chatId = update.chatId)
+            player = playerRepository.save(player)
+        }
+        return player!!
+    }
+
+    fun getGameByPlayer(player: Player) = games[player.chatId]
+
+    fun addPlayerInQuery(player: Player, games: String) = query[games]!!.add(player)
 
 }
