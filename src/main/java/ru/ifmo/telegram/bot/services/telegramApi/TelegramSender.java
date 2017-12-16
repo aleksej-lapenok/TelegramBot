33878d9cecb18b8a0be64bc3d3caf90d6612649f
CawaEast <777.777.1234@mail.ru>
2017-12-16 04:23:55 +0300

    markdown

--- src/main/java/ru/ifmo/telegram/bot/services/telegramApi/TelegramSender.java
+++ src/main/java/ru/ifmo/telegram/bot/services/telegramApi/TelegramSender.java
@@ -75,6 +75,7 @@ public class TelegramSender {
         List<NameValuePair> nvps = new ArrayList<>();
         nvps.add(new BasicNameValuePair("chat_id", id.toString()));
         nvps.add(new BasicNameValuePair("text", text));
+        nvps.add(new BasicNameValuePair("parse_mode", "Markdown"));
         nvps.add(new BasicNameValuePair("reply_markup", keyboard.toJson().toString()));
         return encodeAndSendRequest("sendMessage", nvps);
     }
