e095ad0285b589c3b6873ff0f19da6f973bc395c
nikitakaberov <nik_inf@mail.ru>
2017-12-16 03:02:41 +0300

    Added game in GameFactory

--- src/main/kotlin/ru/ifmo/telegram/bot/services/main/GameFactory.kt
+++ src/main/kotlin/ru/ifmo/telegram/bot/services/main/GameFactory.kt
@@ -7,17 +7,22 @@ import ru.ifmo.services.game.tictactoe.TTTGameFactory
 import ru.ifmo.services.game.tictactoe.TTTStepFactory
 import ru.ifmo.telegram.bot.services.game.GameFactory
 import ru.ifmo.telegram.bot.services.game.StepFactory
+import ru.ifmo.telegram.bot.services.game.blingame.SeaBattleGameFactory
+import ru.ifmo.telegram.bot.services.game.blingame.SeaBattleStepFactory
 
 @Service
 class MainGameFactory {
     val gameFactories = mutableMapOf<Games, GameFactory<*, *>>(Pair(Games.TTT, TTTGameFactory()),
-            Pair(Games.CHECKERS, CheckersGameFactory()))
+            Pair(Games.CHECKERS, CheckersGameFactory()),
+            Pair(Games.SEABATTLE, SeaBattleGameFactory()))
 
     fun getGameFactory(name: String) = getGameFactory(Games.valueOf(name))
 
     fun getGameFactory(games: Games) = gameFactories[games]
 
-    val stepFactories = mutableMapOf<Games, StepFactory<*>>(Pair(Games.TTT, TTTStepFactory()), Pair(Games.CHECKERS, CheckersStepFactory()))
+    val stepFactories = mutableMapOf<Games, StepFactory<*>>(Pair(Games.TTT, TTTStepFactory()),
+            Pair(Games.CHECKERS, CheckersStepFactory()),
+            Pair(Games.SEABATTLE, SeaBattleStepFactory()))
 
     fun getStepFactory(name: String) = getStepFactory(Games.valueOf(name))
 
@@ -26,5 +31,6 @@ class MainGameFactory {
 
 enum class Games {
     TTT,
-    CHECKERS
+    CHECKERS,
+    SEABATTLE
 }
